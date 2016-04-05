be13c68b2537e640eaea76defcd898de5d2d9667
kburckin <kburckin@gmail.com>
2016-04-05 12:55:40 -0700

    Added pwd and shell prompt functionality; need to test further

--- project1/main.cpp
+++ project1/main.cpp
@@ -9,10 +9,8 @@
 
 using namespace std;
 
-
 void SetNonCanonicalMode(int fd, struct termios *savedattributes){
     struct termios TermAttributes;
-    char *name;
     
     // Make sure stdin is a terminal. 
     if(!isatty(fd)){
@@ -35,17 +33,79 @@ void ResetCanonicalMode(int fd, struct termios *savedattributes){
     tcsetattr(fd, TCSANOW, savedattributes);
 }
 
+void printNewLine() {
+    char newLine = 0x0A;
+    write(STDOUT_FILENO, &newLine, 1);
+}
+
+void executePwd() { // to be forked?
+    char *directoryName = NULL;
+    directoryName = getcwd(directoryName, 0);
+    printNewLine();
+    write(STDOUT_FILENO, directoryName, strlen(directoryName));
+    printNewLine();
+}
+
+void printShellPrompt() {
+    char *path = NULL;
+    path = getcwd(path, 0);
+    
+    char *temp;
+    char *currentDirectory = new char [strlen(path)+1];
+    
+    if (strlen(path) > 16) {
+        temp = "/.../";
+        write(STDOUT_FILENO, temp, strlen(temp));
+        
+        temp = strtok(path, "/");
+        strcpy(currentDirectory, temp);
+        while (temp != NULL)
+        {
+            strcpy(currentDirectory, temp);
+            temp = strtok (NULL, "/");
+        }
+        
+        write(STDOUT_FILENO, currentDirectory, strlen(currentDirectory));
+        
+    }
+    else {
+        write(STDOUT_FILENO, path, strlen(path));
+    }
+    
+    temp = "%";
+    write(STDOUT_FILENO, temp, strlen(temp));
+}
+
+
+void directCommand(string command) {
+    if (command == "cd") {
+        
+    }
+    else if (command == "ls") {
+        
+    }
+    else if (command == "pwd") {
+        executePwd();
+    }
+    else if (command == "ff") {
+        
+    }
+    else if (command == "exit") {
+        printNewLine();
+    }
+}
+
 int main() {
 	struct termios SavedTermAttributes;
 	SetNonCanonicalMode(STDIN_FILENO, &SavedTermAttributes);
 
 	char nextChar;
-	//commands
-	//while(1){ //not exit
-		//characters
-		
+    string command;
+	while(command != "exit"){ //breaks on exit if statement
+                // print shell prompt
+                printShellPrompt();
                 read(STDIN_FILENO, &nextChar, 1);
-                
+                command = "";
                 while(nextChar != 0x0A){//not enter
                     switch(nextChar) {
                         case 0x08: //backspace
@@ -58,21 +118,29 @@ int main() {
                             break;
                         }
                         default: { // input chars
-                            cout << "other char" << endl;
+                            //cout << "other char" << endl;
+                            write(STDOUT_FILENO, &nextChar, 1);
+                            command += nextChar;
+                            //cout << command << endl;
+                            
                             break;
                         }
 
                     }
-
-
                     read(STDIN_FILENO, &nextChar, 1);
 
                 }
-	//}
+        directCommand(command);
+	}
 
 	ResetCanonicalMode(STDIN_FILENO, &SavedTermAttributes);
         return 1;
 }
 
+
+
+
 //References:
 //  Nitta noncanmode.c
+// http://www.ascii-code.com/
+
