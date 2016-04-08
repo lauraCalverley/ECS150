86624d0e6218568534cf9b57aa2a6df059b7ae73
Laura Calverley <laura.calverley24@gmail.com>
2016-04-08 10:48:12 -0700

    parsing pipe command for execvp args

--- project1/main.cpp
+++ project1/main.cpp
@@ -121,6 +121,7 @@ void executePwd(vector<vector<char *> > parsedInput) { // to be forked? yes
         char* command = parsedInput[1][1];
 
         cout << __LINE__ << endl;
+        cout << "command: " << command << endl;
         int size = strlen(command);
         cout << __LINE__ << endl;
         char commandA[size];
