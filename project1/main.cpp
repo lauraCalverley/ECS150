7efe6870fde006cd2ca3af011c5fa56f922095b2
Laura Calverley <laura.calverley24@gmail.com>
2016-04-09 15:10:32 -0700

    added execute ff

--- project1/main.cpp
+++ project1/main.cpp
@@ -291,6 +291,7 @@ void executeFf(vector<vector<char*> > parsedInput, char* directory){
     while((dp = readdir(dir)) != NULL){
         cout << "in the while loop\n";
         cout << dp->d_name << endl;
+        cout << dp->d_name == "." << endl;
         if(dp->d_type == DT_DIR && dp->d_name != "."){
             cout << "in the if\n";
             executeFf(parsedInput, dp->d_name);
