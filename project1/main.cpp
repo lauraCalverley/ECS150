56c7be3757b784f42d6e004bf35feeab34c6ec27
Laura Calverley <laura.calverley24@gmail.com>
2016-04-08 21:07:46 -0700

    added directory identifier to permissions

--- project1/main.cpp
+++ project1/main.cpp
@@ -212,6 +212,9 @@ void executePwd(vector<vector<char *> > parsedInput) { // to be forked? yes
 
 void getPerms(string &perms, struct dirent* dp, struct stat statbuf){
     stat(dp->d_name, &statbuf);
+    if(dp->d_type == "DT_DIR"){
+        perms += 'd';
+    } else {perms += ' ';}
     if(S_IRUSR & statbuf.st_mode){
         perms += 'r';
     } else {perms += '-';}
