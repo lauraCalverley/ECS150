fde9ace5ce145f243e3566323568fe42852cf6b2
Laura Calverley <laura.calverley24@gmail.com>
2016-05-11 18:24:58 -0700

    investigating VMFileWrite()

--- project3/VirtualMachine.cpp
+++ project3/VirtualMachine.cpp
@@ -333,6 +333,7 @@ TVMStatus VMTickCount(TVMTickRef tickref) {
 TVMStatus VMFileWrite(int filedescriptor, void *data, int *length) {
     TMachineSignalState sigState;
     MachineSuspendSignals(&sigState);
+    cout << "in VMFileWrite" << endl;
 
     if ((data==NULL) || (length==NULL)) {
         MachineResumeSignals(&sigState);
