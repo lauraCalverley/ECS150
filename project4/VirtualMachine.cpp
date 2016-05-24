52a4c943aa327c89a5e485dc6f5f615c5d8dfa7f
Laura Calverley <laura.calverley24@gmail.com>
2016-05-23 18:39:56 -0700

    testing FAT vector values

--- project4/VirtualMachine.cpp
+++ project4/VirtualMachine.cpp
@@ -222,7 +222,7 @@ void storeFAT(int fd){
     int k = 0;
     for(int i = 0; i < 16; i++){
         for(int j = 0; j < 8; j++){
-            printf("%x ", FAT[k]);
+            printf("%x*4 ", FAT[k]);
             k++;
         }
         cout << endl;
