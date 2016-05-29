66387efcc8e95c796c8688880193a7ee24a0fd3a
Laura Calverley <laura.calverley24@gmail.com>
2016-05-29 12:08:37 -0700

    testing altered file.c

--- project4/apps/file.c
+++ project4/apps/file.c
@@ -1,18 +1,47 @@
 #include "VirtualMachine.h"
 #include <fcntl.h>
 
+// void VMMain(int argc, char *argv[]){
+//     int FileDescriptor, Length, Offset;
+//     char Buffer[128];
+
+//     VMPrint("VMMain opening test.txt\n");    
+//     VMFileOpen("test.txt", O_CREAT | O_TRUNC | O_RDWR, 0644, &FileDescriptor);
+//     VMPrint("VMMain VMFileOpen returned %d\n", FileDescriptor);
+    
+//     VMPrint("VMMain writing file\n");
+//     Length = 13;
+//     VMFileWrite(FileDescriptor,"Hello world!\n",&Length);
+//     VMPrint("VMMain VMFileWrite returned %d\n", Length);
+//     VMPrint("VMMain seeking file\n");
+//     VMFileSeek(FileDescriptor, 6, 0, &Offset);    
+//     VMPrint("VMMain VMFileSeek offset at %d\n",Offset);
+    
+//     VMPrint("VMMain reading file\n");
+//     Length = sizeof(Buffer);
+//     VMFileRead(FileDescriptor,Buffer,&Length);
+//     VMPrint("VMMain VMFileRead returned %d\n", Length);
+//     if(0 <= Length){
+//         Buffer[Length] = '\0';
+//         VMPrint("VMMain read in \"%s\"\n", Buffer);
+//     }
+//     VMPrint("VMMain closing file\n");
+//     VMFileClose(FileDescriptor);
+//     VMPrint("Goodbye\n");    
+// }
+
 void VMMain(int argc, char *argv[]){
     int FileDescriptor, Length, Offset;
     char Buffer[128];
 
-    VMPrint("VMMain opening test.txt\n");    
-    VMFileOpen("test.txt", O_CREAT | O_TRUNC | O_RDWR, 0644, &FileDescriptor);
+    VMPrint("VMMain opening VirtualMachine.h\n");    
+    VMFileOpen("VIRTUA~1.H", O_CREAT | O_TRUNC | O_RDWR, 0644, &FileDescriptor);
     VMPrint("VMMain VMFileOpen returned %d\n", FileDescriptor);
     
-    VMPrint("VMMain writing file\n");
-    Length = 13;
-    VMFileWrite(FileDescriptor,"Hello world!\n",&Length);
-    VMPrint("VMMain VMFileWrite returned %d\n", Length);
+    // VMPrint("VMMain writing file\n");
+    // Length = 13;
+    // VMFileWrite(FileDescriptor,"Hello world!\n",&Length);
+    // VMPrint("VMMain VMFileWrite returned %d\n", Length);
     VMPrint("VMMain seeking file\n");
     VMFileSeek(FileDescriptor, 6, 0, &Offset);    
     VMPrint("VMMain VMFileSeek offset at %d\n",Offset);
@@ -29,4 +58,3 @@ void VMMain(int argc, char *argv[]){
     VMFileClose(FileDescriptor);
     VMPrint("Goodbye\n");    
 }
-
